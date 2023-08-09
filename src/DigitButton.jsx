ad61fde28b32a886ffdc53ebe57fca5ca920aea3
Edson Junior <edson.soares.jr@outlook.com>
2023-08-08 21:40:36 -0300

    calculadora

---
+++ src/DigitButton.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import { ACTIONS } from "./App"
+
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  )
+}
---
+++ src/OperationButton.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import { ACTIONS } from "./App"
+
+export default function OperationButton({ dispatch, operation }) {
+  return (
+    <button
+      onClick={() =>
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  )
+}
